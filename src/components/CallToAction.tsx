ca4fa06a97f6ee3cba07f1b23bfb48c87ad6ecf5
Joshua Aideloje <aidelojejoshua@gmail.com>
2023-11-09 04:28:31 -0800

    Ok, 5am, it works. For real

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface CallToActionProps {
   main: string;
   sub: string;
@@ -14,7 +12,8 @@ const CallToAction = ({ main, sub, align, intro }: CallToActionProps) => {
       <div className={`flex flex-col items-${align} gap-6`}>
         <h3 className="text-3xl font-semibold md:text-7xl max-w-3xl">{main}</h3>
         <p
-          className={`font-thin text-${align} text-muted-foreground max-w-2xl`}>
+          className={`font-thin text-${align} text-muted-foreground max-w-2xl`}
+        >
           {sub}
         </p>
       </div>
